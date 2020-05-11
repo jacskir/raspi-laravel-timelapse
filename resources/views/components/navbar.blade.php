5fd0b44431d75a7bd046c5f5f3214645a6c8fd85
Jacob Skirrow <jacskir@gmail.com>
2020-05-11 16:35:41 +0100

    Remove power button
    Cannot shut down raspberry pi without sudo permission

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,28 +16,6 @@
                     <a class="nav-link" href="{{ route('hotspot') }}">Hotspot Settings <span class="sr-only">(current)</span></a>
                 </li>
             </ul>
-            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#powerModal"><i class="fas fa-power-off"></i> Power</button>
         </div>
     </div>
 </nav>
-
-<!-- Modal -->
-<div class="modal fade" id="powerModal" tabindex="-1" role="dialog" aria-labelledby="powerModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="powerModalLabel">Power</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                You may be disconnected from the current Raspberry Pi hotspot.
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-warning">Restart</button>
-                <button type="button" class="btn btn-danger">Shut down</button>
-            </div>
-        </div>
-    </div>
-</div>
