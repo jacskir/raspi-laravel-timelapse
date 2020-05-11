aea0beb3edb17cfa4bf984e4ef4a75c4842e0fd4
Jacob Skirrow <jacskir@gmail.com>
2020-05-11 17:33:22 +0100

    Fix missing used Process class

--- app/Http/Controllers/TimelapseController.php
+++ app/Http/Controllers/TimelapseController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\Process\Process;
 
 class TimelapseController extends Controller
 {
